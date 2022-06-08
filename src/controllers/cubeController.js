fbe5df0c79eab4e108507e9ed1797305b196fb2b
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-06-08 21:58:16 +0300

    render static details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,34 +1,30 @@
-const router = require('express').Router();
-const fs = require('fs/promises');
-const path = require('path');
+const router = require("express").Router();
+const cubeService = require("../services/cubeService");
 
-const cubes = require('../db.json');
-
-router.get('/create', (req, res) => {
-    res.render('create');
+router.get("/create", (req, res) => {
+    res.render("create");
 });
 
-router.post('/create', (req, res) => {
+router.post("/create", async (req, res) => {
     const cube = req.body;
 
     //Validate the
     if (cube.name.length < 2) {
-        return res.status(400).send('Invalid request');
+        return res.status(400).send("Invalid request");
     }
 
     //Save Data
-    cubes.push(cube);
-    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), { encoding: 'utf-8' })
-        .then(() => {
-            //Redirect the page
-            res.redirect('/');
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        })
+    try {
+        await cubeService.save(cube)
+        res.redirect("/");
+    } catch (error) {
+        res.status(400).send(error);
+    }
 
-  
+});
 
+router.get('/details/:id', (req, res) => {
+    res.render('details');
 })
 
 module.exports = router;
