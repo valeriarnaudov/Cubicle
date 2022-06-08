458922505bf30d55c1a3d593126c8d7c22913192
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-06-08 22:02:07 +0300

    add dynamic details page

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -2,6 +2,8 @@ const fs = require("fs/promises");
 const path = require("path");
 const cubes = require("../db.json");
 
+exports.getOne = (cubeId) => cubes[cubeId];
+
 exports.save = (cube) => {
     cubes.push(cube);
     let textData = JSON.stringify(cubes, "", 4);
